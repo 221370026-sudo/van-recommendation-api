8fde526fc0bcfaa6ac893b900af383bae84c57f0
221370026-sudo <221370026@gift.edu.pk>
2026-04-24 15:42:07 +0500

    app.py

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
 import pandas as pd
+import os
 
 app = Flask(__name__)
 CORS(app)
 
+# Load model
 model = pickle.load(open("van_model.pkl", "rb"))
 
 @app.route("/")
@@ -21,4 +23,7 @@ def predict():
 
     return jsonify({"scores": scores.tolist()})
 
-# IMPORTANT: no app.run() on Render
+# IMPORTANT for Railway
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
